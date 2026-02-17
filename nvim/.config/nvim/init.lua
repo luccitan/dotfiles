b1553cbbebd6152f0d99dd42faa23040c1152955
Tanguy LUCCI <tanguylucci@protonmail.com>
2026-02-17 11:57:46 +0100

    bunch of nvim config changes, mostly with nvim 0.11 lsp changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,44 +6,18 @@ require "config.autocmds"
 require "config.mappings"
 require "config.settings"
 
-------------------------------------------------------------------------------------------------------------------------
--------------- Plugin Management
-------------------------------------------------------------------------------------------------------------------------
--- Bootstraping lazy.nvim
--- Taken straight ouf of the documentation (november 2024)
--- https://lazy.folke.io/installation
+require "config.lazy"
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Configs are loaded natively by neovim inside lsp/ directory
+-- Most/all of configs are taken from https://github.com/neovim/nvim-lspconfig/tree/master/lsp
+vim.lsp.enable({
+    -- lua
+    "luals",
 
+    -- python
+    "pyright",
+    "ruff",
 
--- Setting up Lazy
-
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    -- This way plugins are imported from nvim/lua/plugins folder
-    -- Lua files within should return table with the plugins to install
-    { import = "plugins" },
-  },
-
-  install = { colorscheme = { "tokyonight" } }, -- default colorscheme during install
-
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+    -- bash
+    "bashls"
 })
-------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------
---
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,34 @@
+-- Bootstraping lazy.nvim
+-- Taken straight ouf of the documentation (november 2024)
+-- https://lazy.folke.io/installation
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setting up Lazy  
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    -- This way plugins are imported from nvim/lua/plugins folder
+    -- Lua files within should return table with the plugins to install
+    { import = "plugins" },
+  },
+
+  install = { colorscheme = { "tokyonight" } }, -- default colorscheme during install
+
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
